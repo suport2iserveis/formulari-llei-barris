f5ce6c3802ffb39e6cf11e89109f80d7f0d4afda
suport2iserveis <suport2@iserveis.cat>
2026-07-29 09:37:07 +0200

    Delete assets directory

--- assets/barrys.config.js
+++
@@ -1,6 +0,0 @@
-window.BARRYS_CONFIG = Object.freeze({
-  // Enganxa aquí l'URL que retorna el desplegament del Worker de la carpeta barrys-worker.
-  endpoint: "",
-  assistantVersion: "1.0.0",
-  requestTimeoutMs: 45000
-});
